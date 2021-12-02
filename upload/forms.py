d3b560566e389dba73b40ad1106e789f168f1886
mh.lee <mh.lee239@gmail.com>
2021-12-02 12:04:44 +0900

    temp page bug fix & db overwriting

--- upload/forms.py
+++ upload/forms.py
@@ -28,23 +28,23 @@ class UserUploadForm(ModelForm):
             'created_at':'',
         }
 
-    def clean(self):  # 요청파라미터 값들 조회
-        cleaned_data = super().clean()  # dictionary 반환.
-        mealtimes = cleaned_data.get('mealtimes')
-        created_at = cleaned_data.get('created_at')
-        check_eaten = UploadResult.objects.filter(eaten_dt=created_at)\
-            .select_related('upload_id').values_list('upload_id__mealtimes',flat=True)
-        try:
-            if mealtimes in check_eaten:
-                if mealtimes == 1:
-                    mealtimes = '아침'
-                elif mealtimes == 2:
-                    mealtimes = '점심'
-                else:
-                    mealtimes = '저녁'
-                self.add_error('mealtimes',f'선택하신 날의 {mealtimes}은 등록이 되어있습니다 :) ')
-        except:
-            pass
+    # def clean(self):  # 요청파라미터 값들 조회
+    #     cleaned_data = super().clean()  # dictionary 반환.
+    #     mealtimes = cleaned_data.get('mealtimes')
+    #     created_at = cleaned_data.get('created_at')
+    #     check_eaten = UploadResult.objects.filter(eaten_dt=created_at)\
+    #         .select_related('upload_id').values_list('upload_id__mealtimes',flat=True)
+    #     try:
+    #         if mealtimes in check_eaten:
+    #             if mealtimes == 1:
+    #                 mealtimes = '아침'
+    #             elif mealtimes == 2:
+    #                 mealtimes = '점심'
+    #             else:
+    #                 mealtimes = '저녁'
+    #             self.add_error('mealtimes',f'선택하신 날의 {mealtimes}은 등록이 되어있습니다 :) ')
+    #     except:
+    #         pass
 
 class UserEatenForm(ModelForm):
     class Meta:
